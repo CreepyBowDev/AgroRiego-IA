8409e3ebc94cf98164e305ac1891593d06505bc7
jesus <jesus@gmail.com>
2026-05-30 10:59:45 -0400

    commit

--- AgroRiego-IA-main/backend/routes/usersRoutes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-
-import { userController } from "../controllers/usersController.js";
-
-export const usersRouter = Router();
-
-usersRouter.get("/", userController.getUsers);
